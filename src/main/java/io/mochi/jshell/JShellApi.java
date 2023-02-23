96c73441c436dd9da77771fe3828a09f1a48006c
Mochis <Mochis@users.noreply.github.com>
2023-02-24 00:27:03 +0100

    Add JShell example

---
+++ src/main/java/io/mochi/jshell/JShellApi.java
@@ -0,0 +1,15 @@
+package io.mochi.jshell;
+
+import jdk.jshell.JShell;
+
+/**
+ * Example of use of JShell API to execute a simple product
+ */
+public class JShellApi {
+
+  public static void main(String[] args) {
+    try(JShell jShell = JShell.create()) {
+      System.out.print(jShell.eval("2 * 2").get(0).value());
+    }
+  }
+}
